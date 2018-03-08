eae0e5bbc0acf6716e5523e04255474636605d0f
Daniel Rodriguez <dannyjose1112@hotmail.com>
2018-03-08 15:48:08 -0600

    [UPDATED] added routes for admin

--- src/app/home/admin/admin.routing.ts
+++ src/app/home/admin/admin.routing.ts
@@ -7,6 +7,9 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RolesComponent } from './roles/roles.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { SucursalesComponent } from './sucursales/sucursales.component';
+import { ModulosComponent } from './modulos/modulos.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -14,7 +17,10 @@ const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'roles', component: RolesComponent },
     { path: 'usuarios', component: UsuariosComponent },
-    { path: 'perfil', component: PerfilComponent }
+    { path: 'modulos', component: ModulosComponent },
+    { path: 'empleados', component: EmpleadosComponent },
+    { path: 'perfil', component: PerfilComponent },
+    { path: 'sucursales', component: SucursalesComponent }
   ]},
   { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
