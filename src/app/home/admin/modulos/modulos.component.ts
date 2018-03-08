cc5b1d0291ef9e7184130ac56805c7eb60d410ed
Daniel Rodriguez <dannyjose1112@hotmail.com>
2018-03-08 15:46:42 -0600

    [ADDED] added modulos component

---
+++ src/app/home/admin/modulos/modulos.component.ts
@@ -0,0 +1,134 @@
+import { Component, OnInit } from '@angular/core';
+import { ModulosService } from "./../_services/modulos.service";
+
+import { NotificationsService } from 'angular2-notifications';
+
+declare var $: any
+
+@Component({
+  selector: 'app-modulos',
+  templateUrl: './modulos.component.html',
+  styleUrls: ['./modulos.component.css']
+})
+export class ModulosComponent implements OnInit {
+  title:string="Modulos"
+  Table:any
+  selectedData:any
+  public rowsOnPage = 10;
+  public search:any
+  icono1:string = 'pencil'
+  constructor(
+    private _service: NotificationsService,
+    private mainService: ModulosService
+  ) { }
+
+  ngOnInit() {
+    this.cargarAll()
+  }
+
+  cargarAll(){
+    $('#Loading').css('display','block')
+    $('#Loading').addClass('in')
+    this.mainService.getAll()
+                      .then(response => {
+                        this.Table = response
+                        $("#editModal .close").click();
+                        $("#insertModal .close").click();
+                        $('#Loading').css('display','none')
+                        console.clear
+                      }).catch(error => {
+                        console.clear
+                        this.createError(error)
+                        $('#Loading').css('display','none')
+                      })
+  }
+
+  insert(formValue:any){
+    $('#Loading').css('display','block')
+    $('#Loading').addClass('in')
+    this.mainService.create(formValue)
+                      .then(response => {
+                        this.cargarAll()
+                        console.clear
+                        this.create('Rol Ingresado')
+                        $('#Loading').css('display','none')
+                        $('#insert-form')[0].reset()
+                      }).catch(error => {
+                        console.clear
+                        this.createError(error)
+                        $('#Loading').css('display','none')
+                      })
+
+
+  }
+
+  cargarSingle(id:number){
+    this.mainService.getSingle(id)
+                      .then(response => {
+                        this.selectedData = response;
+                      }).catch(error => {
+                        console.clear
+                        this.createError(error)
+                      })
+  }
+
+  update(formValue:any){
+    $('#Loading').css('display','block')
+    $('#Loading').addClass('in')
+    //console.log(data)
+    this.mainService.update(formValue)
+                      .then(response => {
+                        this.cargarAll()
+                        console.clear
+                        this.create('Rol Actualizado exitosamente')
+                        $('#Loading').css('display','none')
+                      }).catch(error => {
+                        console.clear
+                        this.createError(error)
+                        $('#Loading').css('display','none')
+                      })
+
+  }
+
+  delete(id:string){
+    $('#Loading').css('display','block')
+    $('#Loading').addClass('in')
+    if(confirm("¿Desea eliminar el Rol?")){
+      this.mainService.delete(id)
+                        .then(response => {
+                          this.cargarAll()
+                          console.clear
+                          this.create('Rol Eliminado exitosamente')
+                          $('#Loading').css('display','none')
+                        }).catch(error => {
+                          console.clear
+                          this.createError(error)
+                          $('#Loading').css('display','none')
+                        })
+    }else{
+      $('#Loading').css('display','none')
+    }
+
+  }
+
+  public options = {
+    position: ["bottom", "right"],
+    timeOut: 2000,
+    lastOnBottom: false,
+    animate: "fromLeft",
+    showProgressBar: false,
+    pauseOnHover: true,
+    clickToClose: true,
+    maxLength: 200
+};
+
+create(success) {
+     this._service.success('¡Éxito!',success)
+
+}
+createError(error) {
+     this._service.error('¡Error!',error)
+
+}
+}
+
